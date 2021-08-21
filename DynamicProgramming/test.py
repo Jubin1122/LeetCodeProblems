34a2986c097446fc582f5ca853090ee69875111a
Jubin1122 <jubin.m.2211@gmail.com>
2021-08-21 17:26:44 +0530

    1. dynamic programming section has started 2. Easy amd hard p[roblems completed

---
+++ DynamicProgramming/test.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        x = str(x)
+        i= 0
+        j =len(x) - 1
+        while i < j or i!=j:
+            if int(x[i]) != int(x[j]):
+                return False
+            i +=1
+            j -=1
+        
+        return True 
+
+
+obj = Solution()
+print(obj.isPalindrome(11))
